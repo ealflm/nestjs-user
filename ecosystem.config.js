2bc9b8a33111cd1390f9abb93f75a9827a7f47ef
NamDP <ealflm@gmail.com>
2024-03-16 16:03:08 +0700

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "NESTJS-ADMIN",
+      name: "NESTJS-USER",
       exec_mode: "cluster",
       instances: "max", // Or a number of instances
       script: "node_modules/next/dist/bin/next",
