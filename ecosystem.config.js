19052d756f58a9dd38be721451677d07d0144b09
NamDP <ealflm@gmail.com>
2024-03-16 22:44:26 +0700

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,43 @@
+const argEnvIndex = process.argv.indexOf("--env");
+let argEnv = (argEnvIndex !== -1 && process.argv[argEnvIndex + 1]) || "";
+
+const RUN_ENV_MAP = {
+  local: {
+    instances: 2,
+    max_memory_restart: "250M",
+  },
+  dev: {
+    instances: 2,
+    max_memory_restart: "250M",
+  },
+  prod: {
+    instances: 4,
+    max_memory_restart: "1000M",
+  },
+};
+
+if (!(argEnv in RUN_ENV_MAP)) {
+  argEnv = "prod";
+}
+
 module.exports = {
   apps: [
     {
-      name: "NESTJS-USER",
-      exec_mode: "cluster",
-      instances: "1", // Or a number of instances
+      name: "you-nextjs-appname",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 4000",
+      args: "start -p 2000",
+      instances: RUN_ENV_MAP[argEnv].instances,
+      exec_mode: "cluster",
+      watch: false,
+      max_memory_restart: RUN_ENV_MAP[argEnv].max_memory_restart,
       env_local: {
-        APP_ENV: "local", // APP_ENV=local
+        APP_ENV: "local",
       },
       env_dev: {
-        APP_ENV: "dev", // APP_ENV=dev
+        APP_ENV: "dev",
       },
       env_prod: {
-        APP_ENV: "prod", // APP_ENV=prod
+        APP_ENV: "prod",
       },
     },
   ],
