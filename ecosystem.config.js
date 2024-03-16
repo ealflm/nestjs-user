19319a24717d7660441fcb54a1e81407a3d59da7
NamDP <ealflm@gmail.com>
2024-03-16 23:00:00 +0700

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ if (!(argEnv in RUN_ENV_MAP)) {
 module.exports = {
   apps: [
     {
-      name: "you-nextjs-appname",
+      name: "NEXTJS-USER",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 2000",
       instances: RUN_ENV_MAP[argEnv].instances,
